9a08f3adee3cd2fc502e38c0838cbe1642c6094c
Dirk Fortmeier <d.fortmeier@gmail.com>
2012-06-16 00:03:29 +0200

    fixed last commit (missing creation of dragon)

--- space-dragon/src/com/chris/spacedragon/Game.java
+++ space-dragon/src/com/chris/spacedragon/Game.java
@@ -17,27 +17,28 @@ public class Game implements ApplicationListener {
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
-	
+
 	public Dragon dragon;
-	
+
 	@Override
-	public void create() {		
+	public void create() {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
-		
-		camera = new OrthographicCamera(1, h/w);
+
+		camera = new OrthographicCamera(1, h / w);
 		batch = new SpriteBatch();
-		
+
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
+
 		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
+
 		sprite = new Sprite(region);
 		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
-		
+		sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
+		sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
+
+		dragon = new Dragon();
 		dragon.create();
 	}
 
@@ -48,15 +49,15 @@ public class Game implements ApplicationListener {
 	}
 
 	@Override
-	public void render() {		
+	public void render() {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
+
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		sprite.draw(batch);
 		batch.end();
-		
+
 		dragon.update();
 		dragon.render();
 	}
